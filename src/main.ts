bd7f26398f4746d58af49ffd969eb2bb9c05fdff
Çağatay <cagatayldzz@gmail.com>
2024-12-22 13:34:26 +0300

    Refactor data processing and enhance type definitions for improved clarity and functionality

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,74 @@
 import * as path from "path";
 import * as fs from "fs";
 
-import type { Warframe, Category, Weapon, Locale } from "warframe-items";
+import type { Locale } from "warframe-items";
 
 import { loadI18nData } from "./utils/i18n";
 import { Translatable } from "./utils/type";
 
-const I18N_FILE = `./node_modules/warframe-items/data/json/i18n.json`;
+const I18N_FILE = "./node_modules/warframe-items/data/json/i18n.json";
 
-const DATAS: { category: Category; outputFile: string }[] = [
-  { outputFile: "./data/warframes.json", category: "Warframes" },
-  { outputFile: "./data/primary.json", category: "Primary" },
+const CATEGORY = [
+  "Arcanes",
+  "Arch-Gun",
+  "Arch-Melee",
+  "Archwing",
+  "Enemy",
+  "Fish",
+  "Gear",
+  "Glyphs",
+  "Melee",
+  "Misc",
+  "Mods",
+  "Node",
+  "Pets",
+  "Primary",
+  "Quests",
+  "Railjack",
+  "Relics",
+  "Resources",
+  "Secondary",
+  "SentinelWeapons",
+  "Sentinels",
+  "Sigils",
+  "Skins",
+  "Warframes",
 ];
+
+const DATA_CONFIG2: { outputFile: string; category: string }[] = CATEGORY.map(
+  (category) => ({
+    outputFile: `./data/${category}.json`,
+    category,
+  })
+);
 
 async function filterData<T extends Translatable>(
   inputFile: string,
   outputFile: string,
   fields: (keyof T)[],
-  i18nMap: Record<
-    string,
-    Record<Locale, { description: string; name: string }>
-  >,
-) {
+  i18nMap: Record<string, Record<Locale, { description: string; name: string }>>
+): Promise<void> {
   try {
     const data: T[] = JSON.parse(fs.readFileSync(inputFile, "utf-8"));
 
     const filteredData = data.map((item) => {
       const result: Partial<Translatable & T> = {};
+
       fields.forEach((field) => {
-        if (field === "imageName" && item.imageName) {
-          (result as any)[field] =
-            `https://raw.githubusercontent.com/WFCD/warframe-items/master/data/img/${item.imageName}`;
-        } else {
-          (result as any)[field] = item[field] ?? "N/A";
-        }
+        result[field] =
+          field === "imageName" && item.imageName
+            ? (`https://raw.githubusercontent.com/WFCD/warframe-items/master/data/img/${item.imageName}` as T[keyof T])
+            : (item[field] ?? undefined);
       });
 
       const i18nEntry = i18nMap[item?.uniqueName];
-      if (i18nEntry) {
-        result.languages = Object.entries(i18nEntry).map(([code, value]) => ({
-          description: value.description || "N/A",
-          name: value.name || "N/A",
-          code: code as Locale,
-        }));
-      } else {
-        result.languages = [];
-      }
+      result.languages = i18nEntry
+        ? Object.entries(i18nEntry).map(([code, value]) => ({
+            description: value.description ?? undefined,
+            name: value.name ?? undefined,
+            code: code as Locale,
+          }))
+        : [];
 
       return result;
     });
@@ -58,39 +81,29 @@ async function filterData<T extends Translatable>(
     fs.writeFileSync(
       outputFile,
       JSON.stringify(filteredData, null, 2),
-      "utf-8",
+      "utf-8"
     );
-    console.log(`Filtered data is written to "${outputFile}"`);
+    console.log(`Filtered data written to "${outputFile}"`);
   } catch (error) {
-    console.error(
-      `Error occurred during data processing (${outputFile}):`,
-      error,
-    );
+    console.error(`Error processing data for "${outputFile}":`, error);
   }
 }
 
-function main() {
+async function main(): Promise<void> {
   const i18nMap = loadI18nData(I18N_FILE);
 
-  DATAS.forEach(async ({ outputFile, category }) => {
-    const inputFile = `./node_modules/warframe-items/data/json/${category}.json`;
+  await Promise.all(
+    DATA_CONFIG2.map(async ({ outputFile, category }) => {
+      const inputFile = `./node_modules/warframe-items/data/json/${category}.json`;
 
-    if (category === "Warframes") {
-      await filterData<Warframe>(
+      await filterData<Translatable>(
         inputFile,
         outputFile,
-        ["description", "uniqueName", "imageName", "armor", "name", "aura"],
-        i18nMap,
+        ["uniqueName", "imageName", "name", "description", "armor", "aura"],
+        i18nMap
       );
-    } else if (category === "Primary") {
-      await filterData<Weapon>(
-        inputFile,
-        outputFile,
-        ["description", "uniqueName", "imageName", "name"],
-        i18nMap,
-      );
-    }
-  });
+    })
+  );
 }
 
 main();
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,9 +1,13 @@
 import { Locale } from "warframe-items";
 
-type Languages = { description: string; name: string; code: Locale }[];
+type Languages = { description?: string; name: string; code: Locale }[];
 
 export type Translatable = {
   languages?: Languages;
+  description: string;
   imageName?: string;
   uniqueName: string;
+  armor: number;
+  name: string;
+  aura: string;
 };
